6859fa295275a87a8e96e16271d6f6113f7abf26
Juan Luis Serrano <juan.serrano@veltech.ai>
2026-04-07 13:06:04 +0000

    feat(weatherReport): creadas herramientas de reporte de clima por ciudad

--- servers/demo_server.py
+++ servers/demo_server.py
@@ -1,5 +1,7 @@
 import os
 from mcp.server.fastmcp import FastMCP
+from duckduckgo_search import DDGS
+import httpx
 
 mcp = FastMCP("Demo MCP Server")
 
@@ -99,6 +101,79 @@ def get_file_stats (filename: str) -> str:
                 )
     except Exception as e:
         return f"ERROR al obtener las estadísticas del archivo: {str(e)}"
+
+
+@mcp.tool()
+def web_search(query: str) -> str:
+    """
+    Busca en internet y devuelve los 5 mejores resultados.
+    """
+    try:
+        with DDGS() as ddgs:
+            results = [r for r in ddgs.text(query, max_results=5)]
+            if not results:
+                return "No se encontraron resultados para esa búsqueda. Continua con las otras tareas."
+
+            texto_final = "Resultados encontrados:\n"
+            for r in results:
+                texto_final += f"- {r['title']}: {r['body']}\n"
+            return texto_final
+    except Exception as e:
+        return f"ERROR en DuckDuckGo: {str(e)}"
+
+
+@mcp.tool()
+async def get_coordinates(location: str) -> str:
+    """
+    Convierte el nombre de una ciudad, municipio o direccion en coordenadas.
+    Ejemplo de uso: get_coordinates("Jodar, España") -> "Latitud: 37.5, Longitud: -3.5"
+    """
+    headers = {"User-Agent": "MCP-Test-Agent/1.0"}
+    async with httpx.AsyncClient(headers=headers) as client:
+        try:
+            url = f"https://nominatim.openstreetmap.org/search?q={location}&format=json&limit=1"
+            resp = await client.get(url)
+            data = resp.json()
+
+            if not data:
+                return f"No se han encontrado coordenadas para la localizacion: '{location}'."
+
+            lat = data[0]["lat"]
+            lon = data[0]["lon"]
+            display_name = data[0]["display_name"]
+
+            return f"Ubicación: {display_name} | Latitud: {lat} | Longitud: {lon}"
+
+        except Exception as e:
+            return f"Error en geolocalización: {str(e)}"
+
+
+@mcp.tool()
+async def get_weather(lat: float, lon: float) -> str:
+    """
+    Obtiene el clima actual dadas una latitud y longitud.
+    """
+    headers = {"User-Agent": "MCP-Test-Agent/1.0"}
+
+    transport = httpx.AsyncHTTPTransport(retries=3)
+
+    async with httpx.AsyncClient(headers=headers, transport=transport, timeout=15.0) as client:
+        try:
+
+            url = f"https://api.open-meteo.com/v1/forecast?latitude={lat}&longitude={lon}&current_weather=true"
+
+            resp = await client.get(url)
+            resp.raise_for_status()
+            data = resp.json()
+
+            curr = data["current_weather"]
+            temp = curr["temperature"]
+            wind = curr["windspeed"]
+
+            return f"Clima actual: {temp}°C con vientos de {wind} km/h."
+
+        except Exception as e:
+            return f"Error técnico al consultar el clima: {type(e).__name__}"
 
 # Ejecución del servidor
 if __name__ == "__main__":
--- src/langchain_agent.py
+++ src/langchain_agent.py
@@ -23,20 +23,33 @@ async def main():
     model = ChatOllama(
         model="qwen2.5:1.5b",
         temperature=0,
+        base_url="http://host.docker.internal:11434"
     )
 
     agent = create_agent(model, tools)
 
     result = await agent.ainvoke(
     {
-"messages": [
+        "messages": [
         {
             "role": "system",
-            "content": "Eres un robot de archivos. SOLO usas herramientas. NO escribas explicaciones. Si te pido algo, usa la tool correspondiente inmediatamente."
+            "content": """Eres un asistente que utiliza las tools para las tareas. 
+            REGLA CRITICA: Si una tarea depende de otra, hazlas en orden y espera el resultado de la primera antes de ejecutar la segunda.
+            Sigue estrictamente este protocolo:
+            1.Analiza la pregunta del usuario y decide qué tools usar.
+            2.No mezcles argumentos de diferentes tools, haz una llamada a la vez.
+            3.Ejecuta las tools en orden logico para resolver la pregunta, si una tool depende de otra, espera su resultado.
+            4.Si una tool retorna un error, pasa a la siguiente sin detener el proceso.
+            5.Responde de forma concisa y no uses memoria si no es necesario. 
+            6.Si el usuario hace una pregunta que no se puede resolver con las tools, responde que no puedes ayudar con esa pregunta."""
         },
         {
             "role": "user",
-            "content": "1. Usa list_files en '.', 'src' y 'servers'. 2. Usa read_project en 'servers/demo_server.py'. 3. Escribe un resumen de lo visto en el proyecto en 'RESUMEN.md' usando write_to_file. Saluda a Juanlu al final."
+            "content": """
+            1.Busca las coordenadas del punto central de 'Jodar, España' usando la tool creada. Espera la respuesta antes de continuar.
+            2.Utiliza las coordenadas obtenidas para ejecutar la tool de clima get_weather y obtener el clima actual de las coordenadas dadas. Espera la respuesta antes de continuar.
+            3.Crea un archivo con los datos obtenidos llamado 'RESUMEN{ciudad}.md' usando la tool write_to_file. 
+            """
         }
     ]
     }
@@ -44,7 +57,6 @@ async def main():
 
     for message in result["messages"]:
         print(message)
-
 
 if __name__ == "__main__":
     asyncio.run(main())
